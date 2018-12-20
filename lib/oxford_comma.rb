ab952b94bd8f95a5caede53475868f4e49d3a999
Sarah Paz <sarah.paz@gmail.com>
2018-12-20 03:23:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0..2].join(" and ")
+  elsif array.length >= 3
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
 end

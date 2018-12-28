fc946ba27ae93574b2ebba492f60841fa1062e52
Sarah Paz <sarah.paz@gmail.com>
2018-12-28 03:26:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
@@ -5,4 +7,5 @@ def oxford_comma(array)
     array[-1].insert(0, "and ")
   end
   array.join(', ')
+  binding.pry
 end

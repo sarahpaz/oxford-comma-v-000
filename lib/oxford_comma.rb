aaa255268f96968d56f05f2d08f1dea2a1efde97
Sarah Paz <sarah.paz@gmail.com>
2018-12-28 03:24:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    array[0]
-  elsif array.length == 2
-    array[0..2].join(" and ")
-  elsif array.length >= 3
-    array[0...-1].join(", ") << ", and #{array[-1]}"
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
   end
+  array.join(', ')
 end
